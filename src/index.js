40d920847d91a66cdf4625b0e05ee35d94421a4b
xycu <smonindemid@gmail.com>
2026-07-24 11:47:49 +0500

    first commit of the build on Webpack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// const burger = require('./modules/burger');
+// import sliderOne from './modules/slider';
+// const sliderTwo = require('./modules/badScript');
+
+// burger();
+// sliderOne();
+// sliderTwo();
