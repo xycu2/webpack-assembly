46d5e445ff1dcf9be532ea4705f4ed9077107018
xycu <smonindemid@gmail.com>
2026-07-26 23:50:54 +0500

    feat: setup devServer, html template, source-maps and ts/js loaders

---
+++ webpack.config.ts
@@ -0,0 +1,48 @@
+import path from "path";
+import HtmlWebpackPlugin  from "html-webpack-plugin";
+import webpack from "webpack";
+import { type Configuration as DevServerConfiguration } from "webpack-dev-server";
+
+type Mode = 'production' | 'development';
+
+interface EnvVariables {
+  mode: Mode,
+  port: number
+}
+
+ export default(env: EnvVariables) => {
+
+  const isDev = env.mode === 'development';
+
+  const config: webpack.Configuration = {
+        mode: env.mode ?? 'development',
+        entry: path.resolve(__dirname, 'src', 'index.js'),
+        output: {
+            path: path.resolve(__dirname, "build"),
+            filename: "[name].[contenthash].js",
+            clean: true,
+        },
+        plugins: [
+            new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
+            isDev && new webpack.ProgressPlugin(),
+        ].filter(Boolean),
+         module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devtool: isDev && 'inline-source-map',
+  devServer: isDev ? {
+    port: env.port ?? 3000,
+    open: true
+  } : undefined,
+    }
+    return config 
+}
