b4f20506f0e2846713d332f4e8fb5aa5da38737f
xycu <smonindemid@gmail.com>
2026-07-27 17:21:28 +0500

    feat: add scss compilation, ts/tsx support and dynamic plugin selection

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,48 +1,69 @@
 import path from "path";
-import HtmlWebpackPlugin  from "html-webpack-plugin";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import { type Configuration as DevServerConfiguration } from "webpack-dev-server";
 
-type Mode = 'production' | 'development';
+type Mode = "production" | "development";
 
 interface EnvVariables {
-  mode: Mode,
-  port: number
+  mode: Mode;
+  port: number;
 }
 
- export default(env: EnvVariables) => {
-
-  const isDev = env.mode === 'development';
+export default (env: EnvVariables) => {
+  const isDev = env.mode === "development";
+  const isProd = env.mode === "production";
 
   const config: webpack.Configuration = {
-        mode: env.mode ?? 'development',
-        entry: path.resolve(__dirname, 'src', 'index.js'),
-        output: {
-            path: path.resolve(__dirname, "build"),
-            filename: "[name].[contenthash].js",
-            clean: true,
+    mode: env.mode ?? "development",
+    entry: path.resolve(__dirname, "src", "index.js"),
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "[name].[contenthash].js",
+      clean: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "public", "index.html"),
+      }),
+      isDev && new webpack.ProgressPlugin(),
+      isProd &&
+        new MiniCssExtractPlugin({
+          filename: "css/[name].[contenthash:8].css",
+          chunkFilename: "css/[name].[contenthash:8].css",
+        }),
+    ].filter(Boolean),
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+          ],
         },
-        plugins: [
-            new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
-            isDev && new webpack.ProgressPlugin(),
-        ].filter(Boolean),
-         module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  devtool: isDev && 'inline-source-map',
-  devServer: isDev ? {
-    port: env.port ?? 3000,
-    open: true
-  } : undefined,
-    }
-    return config 
-}
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    devtool: isDev && "inline-source-map",
+    devServer: isDev
+      ? {
+          port: env.port ?? 3000,
+          open: true,
+        }
+      : undefined,
+  };
+  return config;
+};
