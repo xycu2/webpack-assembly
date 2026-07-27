338016346431530c58be5150787d0dda2d556bb5
xycu <smonindemid@gmail.com>
2026-07-28 01:06:25 +0500

    feat: add css modules

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import styles from './components/Button.module.scss';
+
+// Выведем в консоль, чтобы ты увидал, во что Webpack превратил твои стили!
+console.log('Объект стилей:', styles); 
+// В консоли будет что-то вроде: { btn: "src-Button-module__btn--3x9a1", title: "src-Button-module__title--1a2b3" }
+
+// Создаем элементы через чистый JS
+const container = document.createElement('div');
+
+const title = document.createElement('h1');
+title.textContent = 'Тест CSS Modules в JS';
+// Применяем уникальный класс из объекта!
+title.className = styles.title; 
+
+const button = document.createElement('button');
+button.textContent = 'Нажми меня';
+// Применяем класс кнопки
+button.className = styles.btn; 
+
+container.appendChild(title);
+container.appendChild(button);
+
+document.body.appendChild(container);
